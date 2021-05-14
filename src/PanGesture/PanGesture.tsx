af229127e617bf96f5f1d29f9c134f3134b365c3
gpbaculio <gpbaculio@gmail.com>
2021-05-15 01:21:44 +0800

    add pangesture dir

---
+++ src/PanGesture/PanGesture.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const PanGesture = () => {
+  return (
+    <View>
+      <Text>PanGesture</Text>
+    </View>
+  );
+};
+
+export default PanGesture;
---
+++ src/PanGesture/index.ts
@@ -0,0 +1,1 @@
+export {default as PanGesture} from './PanGesture';
