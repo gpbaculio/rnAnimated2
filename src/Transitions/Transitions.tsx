d9cc5b106b63f2c81b192838f0a70e2aa23c8f38
gpbaculio <gpbaculio@gmail.com>
2021-05-15 03:52:47 +0800

    add Transitions dir

---
+++ src/Transitions/Transitions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Transitions = () => {
+  return (
+    <View>
+      <Text>Transitions</Text>
+    </View>
+  );
+};
+
+export default Transitions;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as WorkletsAndSharedValues} from './WorkletsAndSharedValues';
 export {default as Examples} from './Examples';
 export {default as PanGesture} from './PanGesture';
+export {default as Transitions} from './Transitions';
