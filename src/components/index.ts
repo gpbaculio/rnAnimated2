0126a41fb272cfa16b8c61dbcdc6856361c13d56
gpbaculio <gpbaculio@gmail.com>
2021-05-15 01:46:37 +0800

    add card exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {default as Button} from './Button';
 export {default as ReText} from './ReText';
 export {default as Text} from './Text';
+export {default as Card, Cards, cards, CARD_HEIGHT, CARD_WIDTH} from './Card';
