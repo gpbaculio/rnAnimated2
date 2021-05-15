63a3de36302f34787e043750400b221294a9f980
gpbaculio <gpbaculio@gmail.com>
2021-05-16 02:48:20 +0800

    add higher order dir

---
+++ src/HigherOrder/HigherOrder.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HigherOrder = () => {
+  return (
+    <View>
+      <Text>HigherOrder</Text>
+    </View>
+  );
+};
+
+export default HigherOrder;
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {default as WorkletsAndSharedValues} from './WorkletsAndSharedValues';
 export {default as Examples} from './Examples';
 export {default as PanGesture} from './PanGesture';
 export {default as Transitions} from './Transitions';
+export {default as HigherOrder} from './HigherOrder';
