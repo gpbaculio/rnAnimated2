9fdb8b0fd216598fef6d641f88f712afd89a79e2
gpbaculio <gpbaculio@gmail.com>
2021-05-16 03:24:34 +0800

    add higher order animations

--- src/HigherOrder/HigherOrder.tsx
+++ src/HigherOrder/HigherOrder.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {
+  withTiming,
+  withRepeat,
+  useSharedValue,
+  Easing,
+} from 'react-native-reanimated';
 
-const HigherOrder = () => {
+import {Button} from '../components';
+import {styleGuide} from '../constants';
+import ChatBubble from './ChatBubble';
+import {withPause} from './constants';
+
+const easing = Easing.inOut(Easing.ease);
+
+const Timing = () => {
+  const [play, setPlay] = useState(false);
+
+  const paused = useSharedValue(!play);
+
+  const progress = useSharedValue<null | number>(null);
+
   return (
-    <View>
-      <Text>HigherOrder</Text>
+    <View style={styles.container}>
+      <ChatBubble progress={progress} />
+      <Button
+        label={play ? 'Pause' : 'Play'}
+        primary
+        onPress={() => {
+          setPlay(prev => !prev);
+          paused.value = !paused.value;
+          if (progress.value === null) {
+            progress.value = withPause(
+              withRepeat(withTiming(1, {duration: 1000, easing}), -1, true),
+              paused,
+            );
+          }
+        }}
+      />
     </View>
   );
 };
 
-export default HigherOrder;
+export default Timing;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: styleGuide.palette.background,
+  },
+});
