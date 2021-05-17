58d06a6315a61c8004b8aed3f1cd2c4ecd321622
gpbaculio <gpbaculio@gmail.com>
2021-05-17 15:59:48 +0800

    add circular slider dir setup

---
+++ src/CircularSlider/CircularSlider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CircularSlider = () => {
+  return (
+    <View>
+      <Text>CircularSlider</Text>
+    </View>
+  );
+};
+
+export default CircularSlider;
