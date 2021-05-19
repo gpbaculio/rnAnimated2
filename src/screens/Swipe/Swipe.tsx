41b3fc978d391ea31ce0666b58783a0f5de54f95
gpbaculio <gpbaculio@gmail.com>
2021-05-19 23:45:07 +0800

    add swipe dir

---
+++ src/screens/Swipe/Swipe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Swipe = () => {
+  return (
+    <View>
+      <Text>Swipe</Text>
+    </View>
+  );
+};
+
+export default Swipe;
