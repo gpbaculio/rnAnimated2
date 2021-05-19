e2db859eb28c36b98a0608ed470100dd1eed00f0
gpbaculio <gpbaculio@gmail.com>
2021-05-19 23:44:47 +0800

    add swipe on stack

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
@@ -11,6 +12,7 @@ import {
   HigherOrder,
   CircularSlider,
   GraphInteractions,
+  Swipe,
 } from '../screens';
 
 export type AppStackNavigatorType = {
@@ -21,6 +23,7 @@ export type AppStackNavigatorType = {
   HigherOrder: undefined;
   CircularSlider: undefined;
   GraphInteractions: undefined;
+  Swipe: undefined;
 };
 
 const Stack = createStackNavigator<AppStackNavigatorType>();
@@ -83,6 +86,13 @@ const AppNavigator = () => (
         component={GraphInteractions}
         options={{
           title: 'GraphInteractions',
+        }}
+      />
+      <Stack.Screen
+        name="Swipe"
+        component={Swipe}
+        options={{
+          title: 'Swipe',
         }}
       />
     </Stack.Navigator>
