f214e32aa0cfaef38f505b756da7ddbed056ed98
gpbaculio <gpbaculio@gmail.com>
2021-05-22 04:38:26 +0800

    add animated props + style

--- src/screens/CubicBezier/ControlPoint.tsx
+++ src/screens/CubicBezier/ControlPoint.tsx
@@ -1,24 +1,30 @@
-import React from "react";
-import { View } from "react-native";
+import React from 'react';
+import {View} from 'react-native';
+import Animated, {useAnimatedStyle} from 'react-native-reanimated';
 
 export const CONTROL_POINT_RADIUS = 20;
 
 interface ControlPointProps {
-  x: number;
-  y: number;
+  x: Animated.SharedValue<number>;
+  y: Animated.SharedValue<number>;
   min: number;
   max: number;
 }
 
-const ControlPoint = ({}: ControlPointProps) => {
+const ControlPoint = ({x, y, min, max}: ControlPointProps) => {
+  const style = useAnimatedStyle(() => ({
+    transform: [{translateX: x.value}, {translateY: y.value}],
+  }));
+
   return (
-    <View
+    <Animated.View
       style={[
         {
-          position: "absolute",
+          position: 'absolute',
           width: CONTROL_POINT_RADIUS * 2,
           height: CONTROL_POINT_RADIUS * 2,
         },
+        style,
       ]}
     />
   );
