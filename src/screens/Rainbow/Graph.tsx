5f68f0fbf70fb857370509c4e5e7e85edc8caa85
gpbaculio <gpbaculio@gmail.com>
2021-05-31 14:14:49 +0800

    integrate animated style on graph buttons

--- src/screens/Rainbow/Graph.tsx
+++ src/screens/Rainbow/Graph.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet, Dimensions } from "react-native";
-import * as shape from "d3-shape";
-import Svg, { Path } from "react-native-svg";
-import { scaleLinear } from "d3-scale";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Dimensions} from 'react-native';
+import * as shape from 'd3-shape';
+import Svg, {Path} from 'react-native-svg';
+import {scaleLinear} from 'd3-scale';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
-import { Prices, DataPoints, SIZE } from "./Model";
-import Header from "./Header";
-import Cursor from "./Cursor";
-import data from "./data.json";
+import {Prices, DataPoints, SIZE} from './Model';
+import Header from './Header';
+import Cursor from './Cursor';
+import data from './data.json';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
-const { width } = Dimensions.get("window");
+const {width} = Dimensions.get('window');
 
 const values = data.data.prices as Prices;
 const POINTS = 60;
@@ -18,10 +23,10 @@ const POINTS = 60;
 const buildGraph = (datapoints: DataPoints, label: string) => {
   const priceList = datapoints.prices.slice(0, POINTS);
   const formattedValues = priceList.map(
-    (price) => [parseFloat(price[0]), price[1]] as [number, number]
+    price => [parseFloat(price[0]), price[1]] as [number, number],
   );
-  const prices = formattedValues.map((value) => value[0]);
-  const dates = formattedValues.map((value) => value[1]);
+  const prices = formattedValues.map(value => value[0]);
+  const dates = formattedValues.map(value => value[1]);
   const scaleX = scaleLinear()
     .domain([Math.min(...dates), Math.max(...dates)])
     .range([0, SIZE]);
@@ -43,65 +48,44 @@ const buildGraph = (datapoints: DataPoints, label: string) => {
 
 const graphs = [
   {
-    label: "1H",
+    label: '1H',
     value: 0,
-    data: buildGraph(values.hour, "Last Hour"),
+    data: buildGraph(values.hour, 'Last Hour'),
   },
   {
-    label: "1D",
+    label: '1D',
     value: 1,
-    data: buildGraph(values.day, "Today"),
+    data: buildGraph(values.day, 'Today'),
   },
   {
-    label: "1M",
+    label: '1M',
     value: 2,
-    data: buildGraph(values.month, "Last Month"),
+    data: buildGraph(values.month, 'Last Month'),
   },
   {
-    label: "1Y",
+    label: '1Y',
     value: 3,
-    data: buildGraph(values.year, "This Year"),
+    data: buildGraph(values.year, 'This Year'),
   },
   {
-    label: "all",
+    label: 'all',
     value: 4,
-    data: buildGraph(values.all, "All time"),
+    data: buildGraph(values.all, 'All time'),
   },
 ];
 
 const SELECTION_WIDTH = width - 32;
 const BUTTON_WIDTH = (width - 32) / graphs.length;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-  backgroundSelection: {
-    backgroundColor: "#f3f3f3",
-    ...StyleSheet.absoluteFillObject,
-    width: BUTTON_WIDTH,
-    borderRadius: 8,
-  },
-  selection: {
-    flexDirection: "row",
-    width: SELECTION_WIDTH,
-    alignSelf: "center",
-  },
-  labelContainer: {
-    padding: 16,
-    width: BUTTON_WIDTH,
-  },
-  label: {
-    fontSize: 16,
-    color: "black",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
 
 const Graph = () => {
-  const [selected, setSelected] = useState(0);
-  const current = graphs[selected].data;
+  const selected = useSharedValue(0);
+
+  const current = graphs[0].data;
+
+  const style = useAnimatedStyle(() => ({
+    transform: [{translateX: BUTTON_WIDTH * selected.value}],
+  }));
+
   return (
     <View style={styles.container}>
       <Header data={current} />
@@ -118,21 +102,15 @@ const Graph = () => {
       </View>
       <View style={styles.selection}>
         <View style={StyleSheet.absoluteFill}>
-          <View
-            style={[
-              styles.backgroundSelection,
-              { transform: [{ translateX: BUTTON_WIDTH * selected }] },
-            ]}
-          />
+          <Animated.View style={[styles.backgroundSelection, style]} />
         </View>
         {graphs.map((graph, index) => {
           return (
             <TouchableWithoutFeedback
               key={graph.label}
               onPress={() => {
-                setSelected(index);
-              }}
-            >
+                selected.value = withTiming(index);
+              }}>
               <View style={[styles.labelContainer]}>
                 <Text style={styles.label}>{graph.label}</Text>
               </View>
@@ -145,3 +123,31 @@ const Graph = () => {
 };
 
 export default Graph;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  backgroundSelection: {
+    backgroundColor: '#f3f3f3',
+    ...StyleSheet.absoluteFillObject,
+    width: BUTTON_WIDTH,
+    borderRadius: 8,
+  },
+  selection: {
+    flexDirection: 'row',
+    width: SELECTION_WIDTH,
+    alignSelf: 'center',
+  },
+  labelContainer: {
+    padding: 16,
+    width: BUTTON_WIDTH,
+  },
+  label: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
