640d7edfa25fa8ff2c953737aeabe3d50b410ef1
gpbaculio <gpbaculio@gmail.com>
2021-05-31 14:14:24 +0800

    remove footer / move styles to bottom

--- src/screens/Rainbow/Rainbow.tsx
+++ src/screens/Rainbow/Rainbow.tsx
@@ -1,24 +1,23 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 
-import Graph from "./Graph";
-import Footer from "./components/Footer";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "space-between",
-  },
-});
+import Graph from './Graph';
+import Footer from './components/Footer';
 
 const Rainbow = () => {
   return (
     <View style={styles.container}>
       <Graph />
-      <Footer />
     </View>
   );
 };
 
 export default Rainbow;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+  },
+});
