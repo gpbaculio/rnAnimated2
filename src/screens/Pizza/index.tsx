e38d170975a7d887fb835ee2fcdbd91b4cfc8294
gpbaculio <gpbaculio@gmail.com>
2021-06-02 11:20:15 +0800

    add stack for shared element

--- src/screens/Pizza/index.tsx
+++ src/screens/Pizza/index.tsx
@@ -1,1 +1,44 @@
-export {default} from './Pizzas';
+import {RouteProp} from '@react-navigation/core';
+import React from 'react';
+import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
+
+import PizzaChallenge from './PizzaChallenge';
+import Pizzas from './Pizzas';
+
+export type PizzaChallengeRoutes = {
+  Pizzas: undefined;
+  Pizza: {id: string};
+};
+
+const Stack = createSharedElementStackNavigator<PizzaChallengeRoutes>();
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator
+      {...{
+        mode: 'modal',
+        screenOptions: {
+          gestureEnabled: false,
+          headerShown: false,
+          cardOverlayEnabled: true,
+          cardStyle: {
+            backgroundColor: 'transparent',
+          },
+        },
+      }}>
+      <Stack.Screen {...{name: 'Pizzas', component: Pizzas}} />
+      <Stack.Screen
+        {...{
+          name: 'Pizza',
+          component: PizzaChallenge,
+          sharedElements: (route: RouteProp<PizzaChallengeRoutes, 'Pizza'>) => {
+            const {id} = route.params;
+            return [id];
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Navigator;
