7d027216fbe74e1253251717007822f6d2db8411
gpbaculio <gpbaculio@gmail.com>
2021-06-05 05:53:08 +0800

    add expo linear gradient

--- android/app/src/main/java/com/rngestures/generated/BasePackageList.java
+++ android/app/src/main/java/com/rngestures/generated/BasePackageList.java
@@ -7,10 +7,12 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
+        new expo.modules.av.AVPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.gl.GLPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.permissions.PermissionsPackage()
     );
   }
