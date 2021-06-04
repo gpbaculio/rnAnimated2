18f356a2407298afbc37d77a63604b5407f76591
gpbaculio <gpbaculio@gmail.com>
2021-06-04 16:59:00 +0800

    initialize snapchat shared transitions boilerplate

---
+++ src/screens/Snapchat/Story.tsx
@@ -0,0 +1,47 @@
+import {NavigationProp, RouteProp} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet, Dimensions, View, Image} from 'react-native';
+import {PanGestureHandler} from 'react-native-gesture-handler';
+import {Video} from 'expo-av';
+
+import {SnapchatRoutes} from './Model';
+
+interface StoryProps {
+  navigation: NavigationProp<SnapchatRoutes, 'Story'>;
+  route: RouteProp<SnapchatRoutes, 'Story'>;
+}
+
+const {height} = Dimensions.get('window');
+
+const Story = ({route, navigation}: StoryProps) => {
+  const {story} = route.params;
+  return (
+    <View style={[{flex: 1}]}>
+      {!story.video && (
+        <Image
+          source={story.source}
+          style={[
+            {
+              ...StyleSheet.absoluteFillObject,
+              width: undefined,
+              height: undefined,
+            },
+          ]}
+        />
+      )}
+      {story.video && (
+        <Video
+          source={story.video}
+          rate={1.0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={[StyleSheet.absoluteFill]}
+        />
+      )}
+    </View>
+  );
+};
+
+export default Story;
