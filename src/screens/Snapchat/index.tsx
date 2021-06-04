379a5edc9bc2040f2b3a5ddaf931425b35b59995
gpbaculio <gpbaculio@gmail.com>
2021-06-05 03:47:15 +0800

    setup react navigation shared element

--- src/screens/Snapchat/index.tsx
+++ src/screens/Snapchat/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
 
 import Snapchat, {stories} from './Snapchat';
 import StoryComp from './Story';
 import {SnapchatRoutes} from './Model';
+import {RouteProp} from '@react-navigation/core';
 
 export const assets = stories.map(story => [story.avatar, story.source]).flat();
 
-const Stack = createStackNavigator<SnapchatRoutes>();
+const Stack = createSharedElementStackNavigator<SnapchatRoutes>();
 const Navigator = () => (
   <Stack.Navigator
     screenOptions={{
@@ -18,7 +19,16 @@ const Navigator = () => (
     }}
     mode="modal">
     <Stack.Screen name="Snapchat" component={Snapchat} />
-    <Stack.Screen name="Story" component={StoryComp} />
+    <Stack.Screen
+      {...{
+        name: 'Story',
+        component: StoryComp,
+        sharedElementsConfig: route => {
+          const {id} = route.params.story;
+          return [id];
+        },
+      }}
+    />
   </Stack.Navigator>
 );
 
