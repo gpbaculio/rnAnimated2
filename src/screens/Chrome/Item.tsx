ca52628d2c06aa475c212b7e424393e9b4bb38f2
gpbaculio <gpbaculio@gmail.com>
2021-06-04 15:37:59 +0800

    add drag feature on each item

--- src/screens/Chrome/Item.tsx
+++ src/screens/Chrome/Item.tsx
@@ -1,6 +1,13 @@
 import React, {ReactNode} from 'react';
-import {Dimensions, View, ViewStyle} from 'react-native';
-import Animated, {useAnimatedStyle} from 'react-native-reanimated';
+import {Dimensions, StyleSheet, View, ViewStyle} from 'react-native';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useSharedValue} from './Animations';
 
@@ -20,7 +27,22 @@ const Item = ({children, positions, id}: ItemProps) => {
   const position = getPosition(positions.value[id]);
 
   const translateX = useSharedValue(position.x);
+
   const translateY = useSharedValue(position.y);
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    {x: number; y: number}
+  >({
+    onStart: (_, ctx) => {
+      ctx.x = translateX.value;
+      ctx.y = translateY.value;
+    },
+    onActive: ({translationX, translationY}, ctx) => {
+      translateX.value = ctx.x + translationX;
+      translateY.value = ctx.y + translationY;
+    },
+  });
 
   const style = useAnimatedStyle(() => {
     return {
@@ -35,7 +57,15 @@ const Item = ({children, positions, id}: ItemProps) => {
       ],
     };
   });
-  return <Animated.View {...{style}}>{children}</Animated.View>;
+  return (
+    <Animated.View {...{style}}>
+      <PanGestureHandler {...{onGestureEvent}}>
+        <Animated.View style={StyleSheet.absoluteFill}>
+          {children}
+        </Animated.View>
+      </PanGestureHandler>
+    </Animated.View>
+  );
 };
 
 export default Item;
