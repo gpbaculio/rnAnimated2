f757e890a340dec9a9424612186955ff8822b9fa
gpbaculio <gpbaculio@gmail.com>
2021-06-05 03:46:56 +0800

    add gesture event
    where story can be dragged and can be returnedto position on release or stay focused

--- src/screens/Snapchat/Story.tsx
+++ src/screens/Snapchat/Story.tsx
@@ -5,6 +5,16 @@ import {PanGestureHandler} from 'react-native-gesture-handler';
 import {Video} from 'expo-av';
 
 import {SnapchatRoutes} from './Model';
+import {SharedElement} from 'react-navigation-shared-element';
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+import {useSharedValue} from '../Chrome/Animations';
+import {snapPoint} from '../constants';
 
 interface StoryProps {
   navigation: NavigationProp<SnapchatRoutes, 'Story'>;
@@ -13,35 +23,90 @@ interface StoryProps {
 
 const {height} = Dimensions.get('window');
 
+const AnimatedVideo = Animated.createAnimatedComponent(Video);
+
 const Story = ({route, navigation}: StoryProps) => {
   const {story} = route.params;
+
+  const isGestureActive = useSharedValue(false);
+
+  const translateX = useSharedValue(0);
+
+  const translateY = useSharedValue(0);
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: () => {
+      isGestureActive.value = true;
+    },
+    onActive: ({translationX, translationY}) => {
+      translateX.value = translationX;
+      translateY.value = translationY;
+    },
+    onEnd: ({velocityX, velocityY}) => {
+      const shouldGoBack =
+        snapPoint(translateY.value, velocityY, [0, height]) === height;
+      if (shouldGoBack) {
+        runOnJS(navigation.goBack)();
+      } else {
+        translateX.value = withSpring(0, {velocity: velocityX});
+        translateY.value = withSpring(0, {velocity: velocityY});
+      }
+
+      isGestureActive.value = false;
+    },
+  });
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: translateX.value},
+        {translateY: translateY.value},
+      ],
+    };
+  });
+
+  const borderStyle = useAnimatedStyle(() => {
+    return {
+      borderRadius: withTiming(isGestureActive.value ? 24 : 0),
+    };
+  });
+
   return (
-    <View style={[{flex: 1}]}>
-      {!story.video && (
-        <Image
-          source={story.source}
-          style={[
-            {
-              ...StyleSheet.absoluteFillObject,
-              width: undefined,
-              height: undefined,
-            },
-          ]}
-        />
-      )}
-      {story.video && (
-        <Video
-          source={story.video}
-          rate={1.0}
-          isMuted={false}
-          resizeMode="cover"
-          shouldPlay
-          isLooping
-          style={[StyleSheet.absoluteFill]}
-        />
-      )}
-    </View>
+    <PanGestureHandler {...{onGestureEvent}}>
+      <Animated.View style={[styles.container, style]}>
+        <SharedElement {...{id: story.id, style: styles.container}}>
+          {!story.video && (
+            <Animated.Image
+              source={story.source}
+              style={[borderStyle, styles.image]}
+            />
+          )}
+          {story.video && (
+            <AnimatedVideo
+              source={story.video}
+              rate={1.0}
+              isMuted={false}
+              resizeMode="cover"
+              shouldPlay
+              isLooping
+              style={[StyleSheet.absoluteFill, borderStyle]}
+            />
+          )}
+        </SharedElement>
+      </Animated.View>
+    </PanGestureHandler>
   );
 };
 
 export default Story;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+  },
+});
