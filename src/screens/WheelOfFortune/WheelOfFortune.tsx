2f24c9774d0fc68439c096db710863585dc13f16
gpbaculio <gpbaculio@gmail.com>
2021-08-18 03:04:33 +0800

    setup

--- src/screens/WheelOfFortune/WheelOfFortune.tsx
+++ src/screens/WheelOfFortune/WheelOfFortune.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Path, G, Text as RNSVGText, TSpan} from 'react-native-svg';
+import * as d3Shape from 'd3-shape';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
+import color from 'randomcolor';
+import {snap} from '@popmotion/popcorn';
+
+const numberOfSequence = 10;
 
 const WheelOfFortune = () => {
   return (
